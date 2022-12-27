2abfa6d48760feddd5daf681d00b5d0bcacfdc50
Luke Hovarter <lukehsurvey@gmail.com>
2022-12-26 22:01:39 -0500

    fixed width being more than it should

--- travel-blog-site/src/App.vue
+++ travel-blog-site/src/App.vue
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <div class="row">
+  <div class="row" style="margin-left: 0px; margin-right: 0px;">
     <div class="col-2">
       <navbar />
     </div>
