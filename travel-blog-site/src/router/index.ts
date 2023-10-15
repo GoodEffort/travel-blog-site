5e58e772350ec627f249153395d8a022e661b4aa
goodeffort <lukehsurvey@gmail.com>
2023-10-15 17:01:43 -0400

    changed router history mode to be more compatible with my janky apache server

--- travel-blog-site/src/router/index.ts
+++ travel-blog-site/src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TravelAlbum from '@/components/TravelAlbum.vue'
 import Admin from '@/components/Admin.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
