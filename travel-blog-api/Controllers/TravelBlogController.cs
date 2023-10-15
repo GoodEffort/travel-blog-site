e2816cfa87ff1a79fb1186bce351f54c6cc71caa
goodeffort <lukehsurvey@gmail.com>
2023-10-15 18:38:31 -0400

    removed debugging statements

--- travel-blog-api/Controllers/TravelBlogController.cs
+++ travel-blog-api/Controllers/TravelBlogController.cs
@@ -24,9 +24,9 @@ public class TravelBlogController : ControllerBase
     public IActionResult Get(string TripName, int page = 0, int pageSize = 10)
     {
         var now = DateTime.Now;
-        Console.WriteLine($"GetPhotos called at {now}");
+        // Console.WriteLine($"GetPhotos called at {now}");
         var photos = _repo.Get(TripName, page, pageSize);
-        Console.WriteLine($"GetPhotos returned at {DateTime.Now}, took {DateTime.Now - now}");
+        // Console.WriteLine($"GetPhotos returned at {DateTime.Now}, took {DateTime.Now - now}");
         return Ok(photos);
     }
 
